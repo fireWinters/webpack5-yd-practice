d9b0695847c539d83a2e6242ffeb89bd76c7065b
tangtang <1987567948@qq.com>
2022-03-06 11:38:51 +0800

    新增：增加swc-loader配置在webpack

--- config/webpack.config.js
+++ config/webpack.config.js
@@ -2,27 +2,35 @@ const path = require('path');
 
 module.exports = {
   mode: "development",
-  
+
   entry: "./src/index.js",
-  
+
   output: {
-    filename: '[name].js',
-    path: path.resolve(__dirname, '../dist'),
+    filename: "[name].js",
+    path: path.resolve(__dirname, "../dist"),
   },
   module: {
-      rules:[
-          {
-            test:/\.(ts|tsx)$/i, 
-            loader: 'ts-loader',
-            exclude: ["/node_modules/"],
-          },
-          {
-              test:/\.(eot|svg|ttf|woff|woff2|png|jpg|gif)$/i,
-              type:"asset"
-          }
-      ]
+    rules: [
+      {
+        test: /\.(ts|tsx)$/i,
+        loader: "ts-loader",
+        exclude: ["/node_modules/"],
+      },
+      {
+        test: /\.(eot|svg|ttf|woff|woff2|png|jpg|gif)$/i,
+        type: "asset",
+      },
+      {
+        test: /\.m?js$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          // Use `.swcrc` to configure swc
+          loader: "swc-loader",
+        },
+      },
+    ],
   },
-  resolve:{
-        extensions:[".ts",".tsx",".js"],
-  }
+  resolve: {
+    extensions: [".ts", ".tsx", ".js"],
+  },
 };
