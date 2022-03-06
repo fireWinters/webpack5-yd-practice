46b9a82dc54de78600c5472655a45f3f9dc48db6
tangtang <1987567948@qq.com>
2022-03-06 15:54:39 +0800

    修改：修改入口文件

--- src/ether.js
+++ src/ether.js
@@ -1,13 +1,9 @@
-import Web3 from "web3";
 import WalletConnectProvider from "@walletconnect/web3-provider";
 
 //  Create WalletConnect Provider
 const provider = new WalletConnectProvider({
-  infuraId: "27e484dcd9e3efcfd25a83a78777cdf1", // Required
+  infuraId: "27e484dcd9e3efcfd25a83a78777cdf1",
 });
 
 //  Enable session (triggers QR Code modal)
 await provider.enable();
-
-//  Create Web3
-const web3 = new Web3(provider);
